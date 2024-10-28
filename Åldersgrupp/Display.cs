27e00a74f958d897eb0613ec5750990c214cec1d
Fakhara <fakhara.imran82@gmail.com>
2024-10-28 17:48:01 +0100

    övning Åldersgrupp

---
+++ Åldersgrupp/Display.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Åldersgrupp
+{
+    public static class Display
+    {
+        public static void DisplayPeople(string description, List<Person> people, FilterDelegate filter)
+        {
+            Console.WriteLine(description); 
+            foreach (var person in people)
+            {
+                if(filter(person))
+                {
+                    Console.WriteLine($"{person.Name} ({person.Age} år)"); ;
+                }
+            }
+            Console.WriteLine();
+        }
+        public static bool IsChild(Person person) => person.Age < 18;
+
+        public static bool IsAdult(Person person) => person.Age >= 18 && person.Age < 65;
+
+        public static bool IsPensioner(Person person) => person.Age >= 65;
+    }
+}
---
+++ Åldersgrupp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Åldersgrupp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>
+            {
+                new Person("Alice", 25),
+                new Person("Bob", 45),
+                new Person("Charlie", 15),
+                new Person("Diana", 75)
+
+            };
+            Display.DisplayPeople("Barn:", people, Display.IsChild);
+            Display.DisplayPeople("Vuxna:", people, Display.IsAdult);
+            Display.DisplayPeople("Pensionærer:", people, Display.IsPensioner);
+        }
+    }
+}
